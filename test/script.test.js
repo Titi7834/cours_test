0bf24283ef0927c6ed34b53fe18db3d0a548a8aa
Titi7834 <tiago.dc78@gmail.com>
2025-10-01 10:12:13 +0200

    second test wrong

--- test/script.test.js
+++ test/script.test.js
@@ -3,3 +3,7 @@ const { addition } = require('../script.js');
 test("adds 1 + 2 to equal 3", () => {
     expect(addition(1, 2)).toBe(3);
 });
+
+test("adds 2 + 2 to equal 3", () => {
+    expect(addition(2,2)).toBe(3);
+});
